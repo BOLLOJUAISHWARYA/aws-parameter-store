d8c9e11053db69a2b9d686475316b5de7a4dadcc
BOLLOJUAISHWARYA <bolloju.aishwarya@gmail.com>
2023-12-14 00:21:20 +0530

    aws

--- vars.tf
+++ vars.tf
@@ -1,6 +1,6 @@
 variable "parameters" {
   default = [
-    {name = "dev.rds.username" , type = "String" , value ="admin1"},
-    {name = "dev.rds.password" , type = "SecureString" , value ="ExpenseApp123"}
+    {name = "dev.rds.username" , type = "String" , value =""},
+    {name = "dev.rds.password" , type = "SecureString" , value =""}
   ]
 }
